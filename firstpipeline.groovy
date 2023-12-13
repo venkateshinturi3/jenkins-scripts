593afa5fa90a9c81acc237e5eb30bace90ab7b73
venkatesh <iamvenkat789@gmail.com>
2023-12-13 08:58:06 +0530

    First Jenkins Shell pipeline

---
+++ firstpipeline.groovy
@@ -0,0 +1,27 @@
+pipeline {
+    agent any
+    stages {
+        stage('Build') {
+            steps {
+                sh 'echo "Hello World"'
+            }
+        }
+        stage('compiling') {
+            steps {
+                sh 'echo "compiling"'
+            }
+        }
+        stage('packaging')
+            steps {
+                sh 'echo"packaging"'
+                }
+        stage('Running test cases')
+            steps {
+                sh 'echo "Test case excution is in progress"'
+            }
+        stage('Deployment')
+            steps {
+                sh 'echo "deployment is completed"'
+            }
+    }
+}
