4757587d58fda580d78ecb7eb07863c1afdb87d1
venkateshinturi3 <venkatesh.dvps@gmail.com>
2023-12-15 11:34:09 +0530

    Added script step

--- firstpipeline.groovy
+++ firstpipeline.groovy
@@ -3,25 +3,35 @@ pipeline {
     stages {
         stage ('Build') {
             steps {
+                script{
                 sh 'echo "Hello World"'
+                }
             }
         }
         stage ('compiling') {
             steps {
+                script{
                 sh 'echo "compiling"'
+                }
             }
         }
         stage ('packaging')
             steps {
+                script{
                 sh 'echo"packaging"'
+                }
                 }
         stage ('Running test cases')
             steps {
+                script{
                 sh 'echo "Test case excution is in progress"'
+                }
             }
         stage ('Deployment')
             steps {
+                script{
                 sh 'echo "deployment is completed"'
+                }
             }
     }
 }
